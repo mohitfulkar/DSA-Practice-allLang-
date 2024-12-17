b91c0b6c03d5fdc04d0923d92d5c600979fb2388
satyam330 <satyamjha9132@gmail.com>
2024-12-17 23:47:57 +0530

    removed comment and created new method

---
+++ JAVA/SelctionSort.java
@@ -0,0 +1,14 @@
+//import java.lang.reflect.Array;
+import java.util.Arrays;
+
+package JAVA ;
+public class SelctionSort {
+    public static void main(String[] args) {
+        int [] array ={2,9,40,53,99,1};
+       Arrays.sort(array);
+
+       for (int num : array) {
+        System.out.print(num + " ");
+    }
+        }
+}
