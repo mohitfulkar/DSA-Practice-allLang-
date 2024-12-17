fe4918a78a577ae6eb9528cafad328770e8591d6
satyam330 <satyamjha9132@gmail.com>
2024-12-17 23:44:54 +0530

    removed comment and created new method

--- JAVA/TwoSum.java
+++ JAVA/TwoSum.java
@@ -1,19 +1,47 @@
-package JAVA;
+// package JAVA;
 
-public class TwoSum {
-    public static void main(String[] args) {
-        int[] nums = {1, 3, 5, 6, 2};
+// public class TwoSum {
+//     public static void main(String[] args) {
+//         int[] nums = {1, 3, 5, 6, 2};
+//         int target = 9;
+//         for (int i = 0; i < nums.length; i++) {
+//             for (int j = i + 1; j < nums.length; j++) {
+//                 if (nums[i] + nums[j] == target) {
+//                    System.out.println("Two numbers are " + nums[i] + " and " + nums[j]);
+//                       return; 
+//                 }
+//             }
+//         }
+
+//         System.out.println("No two numbers added:");
+//     }
+// }
+
+
+ package JAVA;
+ public class TwoSum {
+ 
+    public static int[] twoSum (){
+        int nums[]= {1, 3, 5, 6, 2};
+        int n=nums.length;
         int target = 9;
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    System.out.println("Two numbers are " + nums[i] + " and " + nums[j]);
-                  //  System.out.println("Indices: " + i + ", " + j);
-                    return; 
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]+nums[j]==target){
+                    int a[]={i,j};
+                    return a;
                 }
             }
         }
-
-        System.out.println("No two numbers added:");
+        return null ;
+    }
+    public static void main(String[] args) {
+        int result[]=TwoSum.twoSum();
+        if(result != null){
+        System.out.println("the index are: " +  result[0]+ " " + result[1] );
+    }
+    else{
+        System.out.println("No solution found.. ");
     }
 }
+ }
