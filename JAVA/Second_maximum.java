30c1f0aafd1b58259d0eb24a527c4803181a5f46
satyam330 <satyamjha9132@gmail.com>
2024-12-26 23:55:42 +0530

    Second Maximum number in Array

--- JAVA/Second_maximum.java
+++ JAVA/Second_maximum.java
@@ -8,6 +8,11 @@ public class Second_maximum {
         int n =arr.length ;
         int max = arr[0];
         int smax = Integer.MIN_VALUE;
+
+        if(n<2){
+            System.out.println("Array must contain at least 2 numbers.");
+        }
+        
         for(int i=1;i<n;i++){
             if(arr[i]>max){
                 smax=max;
@@ -17,7 +22,11 @@ public class Second_maximum {
                 smax=arr[i];
             }
         }
-        System.out.println("Second Largest Number is:" + smax);
+        if (smax == Integer.MIN_VALUE) {
+            System.out.println("There is no second largest number.");
+        } else {
+            System.out.println("Second Largest Number is: " + smax);
+        }
     }
 }
  
