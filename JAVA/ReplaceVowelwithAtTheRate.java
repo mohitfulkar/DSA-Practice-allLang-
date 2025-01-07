1ae8a2f3e56bf529fdd5e1bdc4e06ba9076225a1
diksha-chaurasia2309 <dikshachaurasia2309@gmail.com>
2025-01-07 23:24:09 +0530

    creating file for Replace Vowel with @

---
+++ JAVA/ReplaceVowelwithAtTheRate.java
@@ -0,0 +1,28 @@
+public class ReplaceVowelwithAtTheRate {
+   
+        public static void main(String[] args) {
+            
+            String str = "Diksha Chaurasia";
+            char[] str1 = {'a','e','i','o','u'};
+            
+            char[] s = str.toCharArray();
+            
+            for(int i=0; i<s.length; i++) {
+                for(int j=0; j<str1.length; j++) {
+                    if(s[i] == str1[j]) {
+                        s[i] = '@';
+                    }
+                }
+                
+            }
+            String a = new String(s);
+            System.out.println(a);
+    
+        }
+    
+    }
+    
+    
+    
+    // D@ksh@ Ch@@r@s@@
+    
